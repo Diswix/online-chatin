87b426e0032a74220dcf813067305830912813ec
Diswix <coololeksandra7@gmail.com>
2026-05-10 17:45:05 +0300

    ss

--- server.js
+++ server.js
@@ -3,37 +3,24 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-    const staticDir = path.join(__dirname, 'static'); //
-    let filePath = path.join(staticDir, req.url === '/' ? 'index.html' : req.url); //
-    const extname = path.extname(filePath); //
-    let contentType = 'text/html'; //
-    //
-    switch (extname) { //
-        case '.js': //
-            contentType = 'text/javascript'; //
-            break; //
-        case '.css': //
-            contentType = 'text/css'; //
-            break; //
-        case '.json': //
-            contentType = 'application/json'; //
-            break; //
-    } //
-    //
+    const file = req.url === '/' ? 'index.html' : req.url; //
+    const filePath = path.join(__dirname, 'static', file);
+
+    let contentType = 'text/plain';
+    switch (path.extname(filePath)) { //
+        case '.html': contentType = 'text/html'; break;
+        case '.css': contentType = 'text/css'; break; //
+        case '.js': contentType = 'text/javascript'; break; //
+    }
+
     fs.readFile(filePath, (err, content) => { //
-        if (err) { //
-            if (err.code === 'ENOENT') { //
-                res.writeHead(404, { 'Content-Type': 'text/plain' }); //
-                res.end('Error 404: File Not Found'); //
-            } else { //
-                res.writeHead(500, { 'Content-Type': 'text/plain' }); //
-                res.end(`Server Error: ${err.code}`); //
-            } //
-        } else { //
-            res.writeHead(200, { 'Content-Type': contentType }); //
-            res.end(content, 'utf-8'); //
-        } //
-    }); //
+        if (err) {
+            res.statusCode = 404;
+            return res.end('error 404');
+        }
+        res.writeHead(200, { 'Content-Type': contentType }); //
+        res.end(content);
+    });
 });
 
-server.listen(3000)
+server.listen(3000);
