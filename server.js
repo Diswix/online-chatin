48969bfd17d6adc3aa4e4bcc53dc72e789808695
Diswix <coololeksandra7@gmail.com>
2026-05-10 17:41:48 +0300

    ll

--- server.js
+++ server.js
@@ -2,15 +2,38 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const pathToIndex = path.join(__dirname, 'static', 'index.html');
-const indexHtmlFile = fs.readFileSync(pathToIndex);
-
 const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        return res.end(indexHtmlFile);
-    }
-    res.statusCode = 404;
-    return res.end('error 404');
+    const staticDir = path.join(__dirname, 'static'); //
+    let filePath = path.join(staticDir, req.url === '/' ? 'index.html' : req.url); //
+    const extname = path.extname(filePath); //
+    let contentType = 'text/html'; //
+    //
+    switch (extname) { //
+        case '.js': //
+            contentType = 'text/javascript'; //
+            break; //
+        case '.css': //
+            contentType = 'text/css'; //
+            break; //
+        case '.json': //
+            contentType = 'application/json'; //
+            break; //
+    } //
+    //
+    fs.readFile(filePath, (err, content) => { //
+        if (err) { //
+            if (err.code === 'ENOENT') { //
+                res.writeHead(404, { 'Content-Type': 'text/plain' }); //
+                res.end('Error 404: File Not Found'); //
+            } else { //
+                res.writeHead(500, { 'Content-Type': 'text/plain' }); //
+                res.end(`Server Error: ${err.code}`); //
+            } //
+        } else { //
+            res.writeHead(200, { 'Content-Type': contentType }); //
+            res.end(content, 'utf-8'); //
+        } //
+    }); //
 });
 
-server.listen(3000) 
+server.listen(3000)
