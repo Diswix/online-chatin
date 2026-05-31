5cd7f259229170fef6cac3cfb4ddd6dbedcf8472
Diswix <coololeksandra7@gmail.com>
2026-05-31 17:52:38 +0300

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,44 +1,31 @@
-let currentNickname = "";
-while (!currentNickname || currentNickname.trim() === "") {
-    currentNickname = prompt("Please enter your nickname for the chat:");
-}
-
-const socket = io({
-    auth: {
-        nickname: currentNickname
-    }
-});
+const socket = io();
 
 const messages = document.getElementById('messages');
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 
-socket.on('all_messages', function(msgArray){
-    messages.innerHTML = '';
-    if (msgArray) {
-        msgArray.forEach(message => {
-            let item = document.createElement('li');
-            let author = message.login || 'someone';
-            item.textContent = author + ': ' + message.content;
-            messages.appendChild(item);
-        });
-    }
-    window.scrollTo(0, document.body.scrollHeight);
+socket.on('all_messages', function(msgArray) {
+  messages.innerHTML = ''; 
+  msgArray.forEach(msg => {
+    let item = document.createElement('li');
+    let author = msg.login || 'Анонім'; 
+    item.textContent = author + ': ' + msg.content;
+    messages.appendChild(item);
+  });
+  window.scrollTo(0, document.body.scrollHeight);
 });
 
 form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    if (input.value) {
-        socket.emit('new_message', {
-            content: input.value
-        });
-        input.value = '';
-    }
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('new_message', input.value);
+    input.value = '';
+  }
 });
 
-socket.on('message', function(message){
-    let item = document.createElement('li');
-    item.textContent = message;
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
+socket.on('message', function(msg) {
+  let item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
 });
